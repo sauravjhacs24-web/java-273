a396f93f9e18903de47f568759f6dc0b5c41b0ec
sauravjhacs24-web <sauravjha.cs24@bmsce.ac.in>
2025-12-15 22:37:46 +0530

    StudentSGPA.java

--- StudentSGPA.java
+++ StudentSGPA.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-// Step 2: Subject class
+
 class Subject {
     int subjectMarks;
     int credits;
     int grade;
 }
 
-// Step 3–5: Student class
+
 class Student {
     String name;
     String usn;
@@ -15,15 +15,15 @@ class Student {
     Scanner s = new Scanner(System.in);
     Subject subject[];
 
-    // Constructor
+
     Student() {
-        subject = new Subject[8]; // 8 subjects
+        subject = new Subject[8]; 
         for (int i = 0; i < 8; i++) {
             subject[i] = new Subject();
         }
     }
 
-    // Step 6: Read name and USN
+
     void getStudentDetails() {
         System.out.print("Enter Name: ");
         name = s.nextLine();
@@ -31,7 +31,7 @@ class Student {
         usn = s.nextLine();
     }
 
-    // Step 7: Read marks and credits, calculate grades
+   
     void getMarks() {
         for (int i = 0; i < 8; i++) {
             System.out.println("\nEnter details for Subject " + (i + 1) + ":");
@@ -41,7 +41,7 @@ class Student {
             System.out.print("Enter Credits: ");
             subject[i].credits = s.nextInt();
 
-            // Calculate grade
+            
             subject[i].grade = (subject[i].subjectMarks / 10) + 1;
 
             if (subject[i].grade == 11)
@@ -49,7 +49,7 @@ class Student {
             if (subject[i].grade <= 4)
                 subject[i].grade = 0;
 
-            // Invalid marks check
+           
             if (subject[i].subjectMarks > 100 || subject[i].subjectMarks < 0) {
                 System.out.println("Invalid marks! Setting grade = 0");
                 subject[i].grade = 0;
@@ -57,7 +57,7 @@ class Student {
         }
     }
 
-    // Step 8: Compute SGPA
+ 
     void computeSGPA() {
         int effectiveScore = 0;
         int totalCredits = 0;
@@ -70,7 +70,7 @@ class Student {
         SGPA = (double) effectiveScore / totalCredits;
     }
 
-    // Step 9–10: Display details
+    
     void displayDetails() {
         System.out.println("\n----------------------------");
         System.out.println("Name : " + name);
@@ -80,7 +80,7 @@ class Student {
     }
 }
 
-// Step 9: Main class
+
 public class StudentSGPA {
     public static void main(String[] args) {
         Student st = new Student();
