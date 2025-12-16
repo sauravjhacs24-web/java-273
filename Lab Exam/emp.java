63840bb5456add2d59cb5949aaeff274d4c6d4cc
sauravjhacs24-web <sauravjha.cs24@bmsce.ac.in>
2025-12-16 12:18:15 +0530

    Add files via upload

---
+++ Lab Exam/emp.java
@@ -0,0 +1,48 @@
+import java.util.scanner;
+class employee{
+    int epm_id;
+    string emp_name;
+    int emp_age;
+    int emp_loan_requested;
+    student(int emp_id, string emp_name, int emp_age, int emp_loan_requested){
+        this. epm_id= epm_id;
+        this.emp_name=emp_name;
+        this.emp_age=emp_age;
+        this.emp_loan_requested=emp_loan_requested;
+    }
+}
+void inputdata(){
+    sacanner sc=new scanner(System.in)
+    System.out.println("Enter your emp_id");
+    emp_id=sc.nextInt();
+    System.out.println("Enter your emp_name");
+    emp_name=sc.nextline();
+    System.out.println("Enter your emp_age");
+    emp_age=sc.nextInt();
+    System.out.println("Enter your emp_loan_requested");
+    emp_loan_requested=sc.nextInt();
+}
+void display(){
+    System.out.printl("\n-----Employee Details------");
+    System.out.println("emp_id",+id);
+    System.out.println("emp_name",+name);
+    System.out.println("emp_age",+age);
+    System.out.println("emp_loan_requested",+loan_requested);
+}
+public class emp{
+    public static void main(String[] args) {
+        try{
+            scanner sc= new scanner (System.in)
+            int n;
+            System.out.println("Enter the no of employee");
+            n=sc.nextInt();
+        }
+    }
+ }catch(InterruptedException e){
+    e.
+    thorw Exception (msg:the value of emp_loan_requested is >50000)
+
+             
+        }
+    }
+}
